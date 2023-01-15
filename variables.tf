c2f0f08027620d97e492f3afaced7252c672f3a8
maneeshs <maneesh.sharma@hotmail.com>
2023-01-16 02:12:15 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
 subnet_prefix = "10.0.100.0/24"
-#subnet_prefix = ["10.0.0.4/24"]
-#subnet_prefix = ["10.0.0.0/24","10.0.1.0/24"]
-#subnet_prefix = [{cidr_block="10.0.1.0/24", name="prod_subnet"},{cidr_block="10.0.2.0/24", name="dev_subnet"}]
+#subnet_prefix = ["10.0.0.4/24"]    # String
+#subnet_prefix = ["10.0.0.0/24","10.0.1.0/24"]  # List
+#subnet_prefix = [{cidr_block="10.0.1.0/24", name="prod_subnet"},{cidr_block="10.0.2.0/24", name="dev_subnet"}]   # Objects
